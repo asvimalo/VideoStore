ee293baea70a79f247080ab6b78de32f532991b8
asvimalo <asvimalo@gmail.com>
2017-06-03 13:11:41 +0200

    SetUp VideoStoreTest

--- VideoStore/VideoStore.Tests/VideoStoreTests.cs
+++ VideoStore/VideoStore.Tests/VideoStoreTests.cs
@@ -1,18 +1,26 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VideoStore;
+using NSubstitute;
 
 namespace VideoStore.Tests
 {
+    [TestFixture]
     public class VideoStoreTests 
     {
-        private IRentals Rental;
-        public VideoStoreTests(IRentals r)
+        private IRentals rentals;       
+        private VideoStore sut;
+
+        [SetUp]
+        public void SetUp()
         {
-            Rental = r;
+            rentals = Substitute.For<IRentals>();
+            sut = new VideoStore(rentals);
         }
+        
     }
 }
